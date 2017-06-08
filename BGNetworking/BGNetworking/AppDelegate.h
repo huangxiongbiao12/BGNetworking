31264d33f6c467672b71ffb5aaec22d9cba9dd00
黄雄彪 <huangxiongbiao@1demacbook-pro.local>
2017-06-09 07:36:46 +0800

    代码上传

---
+++ BGNetworking/BGNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BGNetworking
+//
+//  Created by 黄雄彪 on 2017/6/9.
+//  Copyright © 2017年 huangxiongbiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BGNetworking/BGNetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BGNetworking
+//
+//  Created by 黄雄彪 on 2017/6/9.
+//  Copyright © 2017年 huangxiongbiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
